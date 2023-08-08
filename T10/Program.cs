9f8027d4d2605fc29d6ed3423724540eb546d6ee
D&L <omenhorek@gmail.com>
2023-08-08 11:36:02 +0400

    задача 10 решена

---
+++ T10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите трехзначное число");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("NO"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
